e8c217cb9af673277b72a7ef546e162310f682dc
Saleh Sayeem <16103179@iubat.edu>
2020-05-17 23:17:12 +0600

    Initial

---
+++ CrudwithDataAnnotation/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CrudwithDataAnnotation.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Enter Your Name")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [Range(20,120, ErrorMessage = "Age must be between 20 to 120")]
+        public string Age { get; set; }
+        [Required(ErrorMessage = "Email is Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Phone is Required")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+        
+        public DateTime JoiningDate { get; set; }
+
+    }
+}
---
+++ CrudwithDataAnnotation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrudwithDataAnnotation.Startup))]
+namespace CrudwithDataAnnotation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
